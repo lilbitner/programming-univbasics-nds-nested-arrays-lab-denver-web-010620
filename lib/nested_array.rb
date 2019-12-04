b9a113f4dc01f95ebf7e2e48c4d59dd4466288a7
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-04 22:08:39 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,5 +1,3 @@
-# Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
-
 ENNET_HOUSE = [
   "Don Gately",
   "Joelle van Dyne",
@@ -17,21 +15,65 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
-  # Build an array that contains (or, "nests") the residents of The Ennet House
-  # and the Enfield Tennis Academy as provided by the constants
-end
+  assembled_matrix = [
+  ENNET_HOUSE = [ "Don Gately", "Joelle van Dyne",
+  "Pat M.", "Kate Gompert",
+  "Bruce Green"] 
+  ENFIELD_TENNIS_ACADEMY = [
+  "Hal Incandenza", "Lyle",
+  "Gerhard Schtitt", "Mario Incandenza",
+  "Michael Pemulis"]
+  ] 
+end 
+
 
 def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
+  
+ assembled_matrix = [
+  ENFIELD_TENNIS_ACADEMY = [
+  "Gerhard Schtitt", "Hal Incandenza",
+  "Lyle", "Mario Incandenza",
+  "Michael Pemulis"]
+  ENNET_HOUSE = [
+  "Bruce Green", "Don Gately", 
+  "Joelle van Dyne", "Kate Gompert", 
+  "Pat M."] 
+  ] 
 end
 
 def matrix_lookup(matrix, row, column)
-  # Return the matrix's content at row and column
+ assembled_matrix = [
+  ENFIELD_TENNIS_ACADEMY = [
+  "Gerhard Schtitt", 
+  "Hal Incandenza", "Lyle", "Mario Incandenza",
+  "Michael Pemulis"]
+  ENNET_HOUSE = [
+  "Bruce Green", "Don Gately", 
+  "Joelle van Dyne", "Kate Gompert", 
+  "Pat M."] 
+  ] 
+ 
+  assembled_matrix[1][1] 
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
+  
+  assembled_matrix = [
+  ENFIELD_TENNIS_ACADEMY = [
+  "Gerhard Schtitt", 
+  "Hal Incandenza", "Lyle", "Mario Incandenza",
+  "Michael Pemulis"]
+  ENNET_HOUSE = [
+  "Bruce Green", "Don Gately", 
+  "Joelle van Dyne", "Kate Gompert", 
+  "Pat M."] 
+  ] 
+  
+  assembled_matrix[0][2] = "Tiny Ewell" 
+  puts assembled_matrix 
 end
